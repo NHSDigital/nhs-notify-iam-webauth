3a9615c2bc1d0794f66d6c445fdfcc07ed70985b
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-07-18 15:48:27 +0100

    CCM-5680 adding base amplify terraform

---
+++ infrastructure/terraform/components/branch/module_amplify_branch.tf
@@ -0,0 +1,18 @@
+module "amplify_branch" {
+  source      = "../../modules/amp_branch"
+
+  name           = lower(substr(join("", regexall("[a-zA-Z0-9-]+",var.branch_name)),0,25))
+  aws_account_id = var.aws_account_id
+  component      = var.component
+  environment    = var.environment
+  project        = var.project
+  region         = var.region
+  group          = var.group
+
+  cognito_user_pool_client_id               = local.iam.cognito_user_pool["id"]
+  cognito_user_pool_identity_provider_names = local.iam.cognito_user_pool["identity_providers"]
+  amplify_app_id                            = local.iam.amplify["id"]
+  branch                                    = var.branch_name
+  domain_name                               = local.acct.dns_zone["name"]
+  subdomain                                 = var.environment
+}
--- infrastructure/terraform/components/iam/cognito_user_pool_domain.tf
+++ infrastructure/terraform/components/iam/cognito_user_pool_domain.tf
@@ -1,5 +1,4 @@
-# resource "aws_cognito_user_pool_domain" "domain" {
-#   user_pool_id = aws_cognito_user_pool.main.id
-#   domain       = local.acct.dns_zone["name"]
-#   certificate_arn = aws_acm_certificate.main.arn
-# }
+resource "aws_cognito_user_pool_domain" "main" {
+  user_pool_id = aws_cognito_user_pool.main.id
+  domain       = "nhsnotify"
+}
