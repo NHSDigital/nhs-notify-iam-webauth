8d8ab40278fc1a0923c61d7b4116c5fdcf8d241d
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-07-23 09:23:16 +0100

    CCM-5836 tidy

--- infrastructure/terraform/components/acct/route53_delegation_set_main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_route53_delegation_set" "main" {
-  reference_name = "main"
-}
--- infrastructure/terraform/components/acct/route53_zone_subdomain.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_zone" "subdomain" {
-  count = var.subdomain_name != "" ? 1 : 0
-
-  name = var.subdomain_name
-
-  delegation_set_id = aws_route53_delegation_set.main.id
-}
