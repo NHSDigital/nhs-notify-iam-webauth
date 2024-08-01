10d5540c0517f7d13722ab8e15c5490db416e332
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-08-01 14:46:04 +0100

    CCM-5836 fix git head refs

--- infrastructure/terraform/components/branch/null_resource_remove_amplify_branch.tf
+++ infrastructure/terraform/components/branch/null_resource_remove_amplify_branch.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "remove_amplify_branch" {
   # Despite destroying the resource, apparently Amplify thinks we want to keep the branch resrouce around
   triggers = {
-    csi = local.csi
-    amplify_app_id = local.iam.amplify["id"]
+    csi                 = local.csi
+    amplify_app_id      = local.iam.amplify["id"]
     amplify_branch_name = module.amplify_branch.name
   }
 
