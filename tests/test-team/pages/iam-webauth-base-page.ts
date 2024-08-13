d8c92a33e29abb098cd87a4bf578b1e98263b53e
muhammed.salaudeen <muhammed.salaudeen1@nhs.net>
2024-08-13 13:16:25 +0100

    CCM-5913: Add playwright test

---
+++ tests/test-team/pages/iam-webauth-base-page.ts
@@ -0,0 +1,27 @@
+import { Locator, type Page } from '@playwright/test';
+
+export class IamWebAuthBasePage {
+  readonly page: Page;
+
+  readonly pageHeader: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.pageHeader = page
+      .locator('[class="nhsuk-heading-xl"]')
+      .and(page.locator('h1'));
+  }
+
+  async navigateTo(url: string) {
+    await this.page.goto(url, { waitUntil: 'load' });
+  }
+
+  async NavigateWithIntercept(url: string) {
+    await this.page.route('', (route) => {
+      route.request({})
+
+    });
+
+    await this.page.goto(url);
+  }
+}
