666ff83b35bc937472c0319f2e90d589eeee7533
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-08-16 15:17:04 +0100

    adding a depends on for root a record

--- infrastructure/terraform/components/iam/cognito_user_pool_domain.tf
+++ infrastructure/terraform/components/iam/cognito_user_pool_domain.tf
@@ -7,4 +7,6 @@ resource "aws_cognito_user_pool_domain" "custom" {
   user_pool_id    = aws_cognito_user_pool.main.id
   certificate_arn = aws_acm_certificate.cognito.arn
   domain          = local.auth_domain_name
+
+  depends_on = [ aws_route53_record.root ]
 }
