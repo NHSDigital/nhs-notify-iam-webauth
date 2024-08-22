a477a17212e88b6b402bb612742b8198d1a72d6d
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-08-22 11:32:03 +0100

    fixing test paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className='nhsuk-grid-row' data-testid='page-content-wrapper'>
       <div className='nhsuk-grid-column-two-thirds'>
         <h1 className='nhsuk-heading-xl' data-testid='page-heading'>
-          AIVA2 CCM-6010 Test Branch!
+          Hello World!
         </h1>
       </div>
     </div>
