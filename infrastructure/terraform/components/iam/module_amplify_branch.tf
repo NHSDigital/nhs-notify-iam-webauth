33fe643904da77b022d8580e3ea890b44a7190eb
sidnhs <siddharth.nair1@nhs.net>
2024-08-23 14:15:23 +0100

    CCM-6250: Update modules to point to public github repo

--- infrastructure/terraform/components/iam/module_amplify_branch.tf
+++ infrastructure/terraform/components/iam/module_amplify_branch.tf
@@ -1,20 +1,18 @@
 module "amplify_branch" {
-  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
-
-  name           = "main"
-  aws_account_id = var.aws_account_id
-  component      = var.component
-  environment    = var.environment
-  project        = var.project
-  region         = var.region
-  group          = var.group
-
-  description = "Amplify branch for main"
-  amplify_app_id                            = aws_amplify_app.main.id
-  branch                                    = "main"
-  stage                                     = "PRODUCTION"
-  enable_auto_build                         = true
-   environment_variables= {
+  source            = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
+  name              = "main"
+  aws_account_id    = var.aws_account_id
+  component         = var.component
+  environment       = var.environment
+  project           = var.project
+  region            = var.region
+  group             = var.group
+  description       = "Amplify branch for main"
+  amplify_app_id    = aws_amplify_app.main.id
+  branch            = "main"
+  stage             = "PRODUCTION"
+  enable_auto_build = true
+  environment_variables = {
     USER_POOL_CLIENT_ID = aws_cognito_user_pool_client.main.id
     NOTIFY_SUBDOMAIN    = var.environment
     NEXT_PUBLIC_BASE_PATH = "/auth"
