5e3baab22af0c02ee69eb8a2e886d2b4d5ab2d76
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-08-27 11:18:56 +0100

    sprint_review_hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className='nhsuk-grid-row' data-testid='page-content-wrapper'>
       <div className='nhsuk-grid-column-two-thirds'>
         <h1 className='nhsuk-heading-xl' data-testid='page-heading'>
-          5777 65 1173!
+          Hello Sprint Review!
         </h1>
       </div>
     </div>
