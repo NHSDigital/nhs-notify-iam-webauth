228454f8674507c9269eecabae9fd75704bb6b25
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-09-02 13:47:05 +0100

    CCM-6413 enable feature auto build

--- infrastructure/terraform/components/branch/module_amplify_branch.tf
+++ infrastructure/terraform/components/branch/module_amplify_branch.tf
@@ -1,16 +1,21 @@
 module "amplify_branch" {
-  source         = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
-  name           = local.normalised_branch_name
+  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
+
+  name         = local.normalised_branch_name
+  display_name = local.normalised_branch_name
+  description  = "Amplify branch for ${local.normalised_branch_name}"
+
   aws_account_id = var.aws_account_id
   component      = var.component
   environment    = var.environment
   project        = var.project
   region         = var.region
   group          = var.group
-  description    = "Amplify branch for ${local.normalised_branch_name}"
-  amplify_app_id = local.iam.amplify["id"]
-  branch         = var.branch_name
-  display_name   = local.normalised_branch_name
+
+  amplify_app_id    = local.iam.amplify["id"]
+  branch            = var.branch_name
+  enable_auto_build = true
+
   environment_variables = {
     USER_POOL_CLIENT_ID   = local.iam.cognito_user_pool_client["id"]
     NOTIFY_SUBDOMAIN      = var.environment
