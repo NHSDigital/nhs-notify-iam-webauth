35b0e4215494b2ab39c9d0932f1a2498bb0cc74b
Tim Ireland <tim.ireland@hscic.gov.uk>
2024-09-06 16:38:35 +0000

    adding next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
+
+export { handler as GET, handler as POST }
