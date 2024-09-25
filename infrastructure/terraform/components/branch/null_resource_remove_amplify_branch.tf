73760b2cab47ef565d97e693af573b56f7e9db9a
Chris Elliott <chris.elliott17@nhs.net>
2024-09-25 13:55:55 +0100

    app id does not trigger null resource

--- infrastructure/terraform/components/branch/null_resource_remove_amplify_branch.tf
+++ infrastructure/terraform/components/branch/null_resource_remove_amplify_branch.tf
@@ -1,8 +1,7 @@
 resource "null_resource" "remove_amplify_branch" {
-  # Despite destroying the resource, apparently Amplify thinks we want to keep the branch resrouce around
+  # Despite destroying the resource, apparently Amplify thinks we want to keep the branch resource around
   triggers = {
     csi                 = local.csi
-    amplify_app_id      = local.app.amplify["id"]
     amplify_branch_name = module.amplify_branch.name
   }
 
