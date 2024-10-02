2cccacea6062d6613b2d13636911b527d9fd4024
Chris Elliott <chris.elliott17@nhs.net>
2024-10-02 09:45:59 +0100

    Add logging

--- app/signout/Redirect.tsx
+++ app/signout/Redirect.tsx
@@ -6,9 +6,16 @@ import { useSearchParams } from 'next/navigation';
 export const Redirect = () => {
   const searchParams = useSearchParams();
 
+  console.log(
+    'signout searchParams',
+    searchParams.get('redirect'),
+    searchParams.entries().next()
+  );
+
   const redirect = searchParams.get('redirect') ?? '/';
 
   useEffect(() => {
+    console.log('signout useeffect', redirect);
     location.href = redirect;
   }, [redirect]);
 
