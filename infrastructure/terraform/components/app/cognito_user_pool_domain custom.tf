2998cbcb8d311eac08d5b3656d34f6721e383e65
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-10-14 15:08:55 +0100

    CCM-7074 fixing globally user pool domain

---
+++ infrastructure/terraform/components/app/cognito_user_pool_domain custom.tf
@@ -0,0 +1,7 @@
+resource "aws_cognito_user_pool_domain" "custom" {
+  user_pool_id    = aws_cognito_user_pool.main.id
+  certificate_arn = aws_acm_certificate.cognito.arn
+  domain          = local.auth_domain_name
+
+  depends_on = [aws_route53_record.root]
+}
--- infrastructure/terraform/components/app/cognito_user_pool_domain.tf
+++ infrastructure/terraform/components/app/cognito_user_pool_domain.tf
@@ -1,12 +1,4 @@
 resource "aws_cognito_user_pool_domain" "main" {
   user_pool_id = aws_cognito_user_pool.main.id
-  domain       = local.csi
-}
-
-resource "aws_cognito_user_pool_domain" "custom" {
-  user_pool_id    = aws_cognito_user_pool.main.id
-  certificate_arn = aws_acm_certificate.cognito.arn
-  domain          = local.auth_domain_name
-
-  depends_on = [aws_route53_record.root]
+  domain       = local.csi_global
 }
