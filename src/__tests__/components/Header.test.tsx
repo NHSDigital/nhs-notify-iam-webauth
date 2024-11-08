b56de264d2d2a5a36c6f4fe59297f08377f9de4a
ben.hansell1 <ben.hansell1@nhs.net>
2024-11-08 12:58:19 +0000

    CCM-7248: get repo into deployable state

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,37 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import { NHSNotifyHeader } from '@/src/components/molecules/Header/Header';
-import { Authenticator } from '@aws-amplify/ui-react';
+
+jest.mock('@/src/components/molecules/LoginStatus/LoginStatus');
 
 describe('Header component', () => {
+  const ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...ENV };
+  });
+
+  afterAll(() => {
+    process.env = ENV;
+  });
+
   it('renders component correctly', async () => {
-    const container = render(
-      <Authenticator.Provider>
-        <NHSNotifyHeader />
-      </Authenticator.Provider>
-    );
-    const ENV = process.env;
+    const container = render(<NHSNotifyHeader />);
 
-    beforeEach(() => {
-      jest.resetModules();
-      process.env = { ...ENV };
-    });
+    expect(container.asFragment()).toMatchSnapshot();
+  });
 
-    afterAll(() => {
-      process.env = ENV;
-    });
-    it('renders component correctly', async () => {
-      render(<NHSNotifyHeader />);
+  it('should not render login link', () => {
+    process.env.NEXT_PUBLIC_DISABLE_CONTENT = 'true';
 
-      await screen.findByText('Sign in');
+    render(<NHSNotifyHeader />);
 
-      expect(container.asFragment()).toMatchSnapshot();
-    });
-
-    it('should not render login link', () => {
-      process.env.NEXT_PUBLIC_DISABLE_CONTENT = 'true';
-      render(<NHSNotifyHeader />);
-
-      expect(screen.queryByTestId('login-link')).not.toBeInTheDocument();
-    });
+    expect(screen.queryByTestId('login-link')).not.toBeInTheDocument();
   });
 });
