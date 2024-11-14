05d9ec98b2bbd6a452b44f1049b19d8f9abf9369
ben.hansell1 <ben.hansell1@nhs.net>
2024-11-14 15:46:50 +0000

    CCM-7248: bas path

--- src/components/molecules/LoginStatus/LoginStatus.tsx
+++ src/components/molecules/LoginStatus/LoginStatus.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { getBasePath } from '@/utils/get-base-path';
+import { BASE_PATH } from '@/src/utils/constants';
 
 export const LoginStatus = () => {
   const { authStatus } = useAuthenticator();
@@ -11,7 +11,7 @@ export const LoginStatus = () => {
     return (
       <a
         className='nhsuk-account__login--link'
-        href={`${getBasePath()}/signout`}
+        href={`${BASE_PATH}/signout`}
       >
         Log out
       </a>
@@ -19,7 +19,7 @@ export const LoginStatus = () => {
   }
 
   return (
-    <a className='nhsuk-account__login--link' href={`${getBasePath()}`}>
+    <a className='nhsuk-account__login--link' href={BASE_PATH}>
       Log in
     </a>
   );
--- utils/get-base-path.ts
+++
@@ -1,1 +0,0 @@
-export const getBasePath = () => process.env.NEXT_PUBLIC_BASE_PATH ?? '/auth';
