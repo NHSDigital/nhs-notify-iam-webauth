bf56f5d65f91946f27f3a87a447d75b008bb1c4e
Mark Ramsden <mark.ramsden5@nhs.net>
2024-11-22 07:44:08 +0000

    CCM-7650: refine CIS2 attribute mapping

--- infrastructure/terraform/components/app/cognito_identity_provider_cis2.tf
+++ infrastructure/terraform/components/app/cognito_identity_provider_cis2.tf
@@ -10,7 +10,7 @@ resource "aws_cognito_identity_provider" "cis2_idp" {
   provider_type = "OIDC"
 
   provider_details = {
-    authorize_scopes          = "openid profile email"
+    authorize_scopes          = "openid profile email nhsperson associatedorgs"
     client_id                 = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["client_id"]
     client_secret             = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["client_secret"]
     oidc_issuer               = local.cis2_issuer_urls[var.cis2_environment]
@@ -18,11 +18,14 @@ resource "aws_cognito_identity_provider" "cis2_idp" {
   }
 
   attribute_mapping = {
-    email       = "email"
-    username    = "sub"
-    uid         = "uid"
-    name        = "name"
-    given_name  = "given_name"
-    family_name = "family_name"
+    email                     = "email"
+    username                  = "sub"
+    given_name                = "given_name"
+    middle_name               = "middle_names"
+    family_name               = "family_name"
+    preferred_username        = "display_name"
+    "custom:idassurancelevel" = "idassurancelevel"
+    "custom:nhsid_user_orgs"  = "nhsid_user_orgs"
+    "custom:nhsid_useruid"    = "nhsid_useruid"
   }
 }
