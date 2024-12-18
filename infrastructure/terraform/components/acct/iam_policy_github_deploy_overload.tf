9b8abc8e906f55b06c814a43779fbfe7bf90accf
Mark Ramsden <mark.ramsden5@nhs.net>
2024-12-18 06:39:21 +0000

    CCM-7651: update permissions

--- infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
+++ infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
@@ -19,6 +19,7 @@ data "aws_iam_policy_document" "github_deploy" {
       "cloudformation:*",
       "cognito-identity:*",
       "cognito-idp:*",
+      "distribution:*",
       "secretsmanager:*"
     ]
     resources = ["*"]
