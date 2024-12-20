37eb5d4ab73afcbc4f5355dcb8bf3f935594f960
Mark Ramsden <mark.ramsden5@nhs.net>
2024-12-20 07:18:27 +0000

    CCM-7651: only deploy custom domain for main branch

--- infrastructure/terraform/components/app/route53_record_cognito_acm_validation.tf
+++ infrastructure/terraform/components/app/route53_record_cognito_acm_validation.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "cognito_acm_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cognito[0].domain_validation_options :
+    for dvo in local.use_custom_cognito_domain ? aws_acm_certificate.cognito[0].domain_validation_options: [] :
     dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
