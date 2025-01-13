7fc123f598dad7b309caed28c3bbfef3da33632c
Mark Ramsden <mark.ramsden5@nhs.net>
2025-01-13 09:46:30 +0000

    CCM-7847: wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import fetchIntercept from 'fetch-intercept';
 import { basicCredentialsInterceptor } from '../utils/basic-credentials-interceptor';
 import { Redirect } from '@/src/components/molecules/Redirect/Redirect';
 import { Button } from 'nhsuk-react-components';
 import { useSearchParams } from 'next/navigation';
-import { cis2Login } from '../utils/cis2-login';
+import { cis2Login, getCis2AmplifyConfiguration } from '../utils/cis2-login';
 import { cognitoUserPoolsTokenProvider } from '@aws-amplify/auth/cognito';
 import { Amplify } from 'aws-amplify';
 import { defaultStorage } from 'aws-amplify/utils';
+import { getAuthTokens } from '../utils/cis2-token-retriever';
+import { randomUUID } from 'crypto';
 
 
 fetchIntercept.register(basicCredentialsInterceptor);
@@ -18,14 +20,15 @@ fetchIntercept.register(basicCredentialsInterceptor);
 const clientId = Amplify.getConfig().Auth?.Cognito.userPoolClientId;
 const OAUTH_PKCE_KEY = `CognitoIdentityServiceProvider.${clientId}.oauthPKCE`;
 
-const AuthenticatorWrapper = (props: { redirectPath: string }) => {
+const AuthenticatorWrapper = (props: { redirectPath: string, requestRef: string }) => {
+  Amplify.configure(getCis2AmplifyConfiguration(), { ssr: true });
   return withAuthenticator(Redirect, {
     variation: 'default',
     hideSignUp: true,
     components: {
       SignIn: {
         Header: () => (
-          <Button onClick={() => cis2Login(props.redirectPath)}>CIS2</Button>
+          <Button onClick={() => cis2Login(props.redirectPath, props.requestRef)}>CIS2</Button>
         ),
       },
     },
@@ -35,23 +38,32 @@ const AuthenticatorWrapper = (props: { redirectPath: string }) => {
 cognitoUserPoolsTokenProvider.authTokenStore.keyValueStorage?.getItem('')
 
 export default function Page() {
+
+  console.log(JSON.stringify(Amplify.getConfig()));
+  console.log(Amplify.getConfig());
+  // const [requestRef] = useState(randomUUID().toString());
+  
   const searchParams = useSearchParams();
   const redirectPath = searchParams.get('redirectPath');
   const code = searchParams.get('code');
   const state = searchParams.get('state');
 
-  useEffect(() => {
-    if (!code) {
-      return;
-    }
+  
 
-    defaultStorage.getItem(OAUTH_PKCE_KEY)
-      .then((pixy) => console.log(pixy));
-  }, [code]);
+  // useEffect(() => {
+  //   if (!code) {
+  //     return;
+  //   }
+
+
+  //   defaultStorage.getItem(OAUTH_PKCE_KEY)
+  //     .then((codeVerifier) => getAuthTokens(code, codeVerifier || ''))
+  //     .then((tokens) => console.log(`Tokens ${JSON.stringify(tokens)}`));
+  // }, [code]);
 
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <AuthenticatorWrapper redirectPath={redirectPath || ''} />
+      <AuthenticatorWrapper redirectPath={redirectPath || ''} requestRef={''} />
     </Suspense>
   );
 }
