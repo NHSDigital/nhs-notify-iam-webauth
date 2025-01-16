8269f2bbd80876b5968d98130552204177f9baea
ClareJonesBJSS <clare.jones75@nhs.net>
2025-01-16 09:15:46 +0000

    CCM-5340: unit test

---
+++ src/__tests__/middleware.test.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from 'next/server';
+import { middleware } from '../middleware';
+
+const cspRegex =
+  /^base-uri 'self'; form-action 'self'; frame-ancestors 'none'; default-src 'none'; connect-src 'self'; font-src 'self' https:\/\/assets.nhs.uk; img-src 'self'; script-src 'self' 'nonce-[\dA-Za-z]+'; style-src 'self'; upgrade-insecure-requests$/;
+
+describe('middleware function', () => {
+  it('sets CSP in response', () => {
+    const request = new NextRequest(
+      new URL('https://main.web-gateway.dev.nhsnotify.national.nhs.uk')
+    );
+    const response = middleware(request);
+    const csp = response.headers.get('Content-Security-Policy');
+    expect(csp).toMatch(cspRegex);
+  });
+});
