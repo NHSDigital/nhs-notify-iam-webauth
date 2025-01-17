ce8d064308ae62cd5f97c32a858d4dbe690fc5e9
Mark Ramsden <mark.ramsden5@nhs.net>
2025-01-17 14:44:23 +0000

    CCM-7847: configure allowed origins

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '/auth';
 const domain = process.env.NOTIFY_DOMAIN_NAME ?? 'localhost:3000';
-// const subdomain = `${basePath}.`.split('~')[1] || '';
-// const allowedOrigins = Array.from(new Set([domain, domain.replace('iam', 'web-gateway'), `${subdomain}${domain}`]).values());
 const allowedOrigins = [domain, domain.replace('iam', 'web-gateway')];
-
-console.log(`allowedOrigins ${allowedOrigins}`);
 
 module.exports = {
   basePath,
