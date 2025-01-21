e5eec5adc12fb8931509b4377a8437e40d05a7a4
Chris Elliott <chris.elliott17@nhs.net>
2025-01-21 15:49:21 +0000

    Update redirect domain

--- infrastructure/terraform/components/app/module_amplify_branch.tf
+++ infrastructure/terraform/components/app/module_amplify_branch.tf
@@ -19,6 +19,6 @@ module "amplify_branch" {
     NEXT_PUBLIC_USER_POOL_CLIENT_ID = aws_cognito_user_pool_client.main.id
     USER_POOL_CLIENT_SECRET         = aws_cognito_user_pool_client.main.client_secret
     NEXT_PUBLIC_COGNITO_DOMAIN      = local.use_custom_cognito_domain ? aws_cognito_user_pool_domain.custom[0].domain : "${aws_cognito_user_pool_domain.main.domain}.auth.eu-west-2.amazoncognito.com"
-    NEXT_PUBLIC_REDIRECT_DOMAIN     = local.auth_gateway_name
+    NEXT_PUBLIC_REDIRECT_DOMAIN     = replace(local.auth_gateway_name, "web-gateway", "iam") # replace is temporary, do not merge
   }
 }
