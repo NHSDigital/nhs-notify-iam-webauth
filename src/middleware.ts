51a8c6c4e6a87d4fab6f85b510242edbc6f23efc
Alex Nuttall <alex.nuttall1@nhs.net>
2025-01-22 15:39:56 +0000

    CCM-5340: disable matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,14 +23,14 @@ export function middleware(request: NextRequest) {
   return response;
 }
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      */
+//     '/((?!_next/static|_next/image|favicon.ico).*)',
+//   ],
+// };
