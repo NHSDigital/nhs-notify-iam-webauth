74ac8863b4923e9024460fcbaa6cb0717fd2b7b3
Chris Elliott <chris.elliott17@nhs.net>
2025-01-28 11:54:50 +0000

    Fix CI errors

--- infrastructure/terraform/components/app/cognito_identity_provider_cis2.tf
+++ infrastructure/terraform/components/app/cognito_identity_provider_cis2.tf
@@ -18,14 +18,14 @@ resource "aws_cognito_identity_provider" "cis2_idp" {
   }
 
   attribute_mapping = {
-    email                         = "email"
-    username                      = "sub"
-    given_name                    = "given_name"
-    middle_name                   = "middle_names"
-    family_name                   = "family_name"
-    preferred_username            = "display_name"
-    "custom:idassurancelevel"     = "idassurancelevel"
-    "custom:nhsid_user_orgs"      = "nhsid_user_orgs"
-    "custom:nhsid_useruid"        = "nhsid_useruid"
+    email                     = "email"
+    username                  = "sub"
+    given_name                = "given_name"
+    middle_name               = "middle_names"
+    family_name               = "family_name"
+    preferred_username        = "display_name"
+    "custom:idassurancelevel" = "idassurancelevel"
+    "custom:nhsid_user_orgs"  = "nhsid_user_orgs"
+    "custom:nhsid_useruid"    = "nhsid_useruid"
   }
 }
