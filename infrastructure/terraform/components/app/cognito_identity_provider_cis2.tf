ee03969ad63bbbca4e346a98c41f13e05ffa28f0
Chris Elliott <chris.elliott17@nhs.net>
2025-01-28 11:45:31 +0000

    Code cleanup

--- infrastructure/terraform/components/app/cognito_identity_provider_cis2.tf
+++ infrastructure/terraform/components/app/cognito_identity_provider_cis2.tf
@@ -27,8 +27,5 @@ resource "aws_cognito_identity_provider" "cis2_idp" {
     "custom:idassurancelevel"     = "idassurancelevel"
     "custom:nhsid_user_orgs"      = "nhsid_user_orgs"
     "custom:nhsid_useruid"        = "nhsid_useruid"
-    "custom:id_assurance_level"   = "id_assurance_level"
-    "custom:auth_assurance_level" = "authentication_assurance_level"
-    "custom:auth_time"            = "auth_time"
   }
 }
