4e2395cc8346b601d47d86d1be16139d1897d4a8
Chris Elliott <chris.elliott17@nhs.net>
2025-02-13 08:53:17 +0000

    Add debug logging

--- src/app/signin/route.ts
+++ src/app/signin/route.ts
@@ -4,6 +4,11 @@ import { cookies } from 'next/headers';
 import { generateCsrf } from '../../utils/csrf-utils';
 
 export const GET = async (request: Request) => {
+  console.log('DEBUG');
+  console.log(request.url);
+  console.log(new URL(request.url));
+  console.log(new URL(request.url).searchParams);
+  console.log(new URL(request.url).searchParams.entries().toArray());
   const redirectPath = new URL(request.url).searchParams.get('redirect') ?? '/';
 
   const csrfToken = await generateCsrf();
