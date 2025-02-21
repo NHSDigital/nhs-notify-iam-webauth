0202dacedb3f9cdb8608f748d91b5128a67372b7
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-02-21 15:30:08 +0000

    CCM-8530 Adding happy path and bad char tests

--- lambdas/backup-cognito-id/src/__tests__/index.test.js
+++ lambdas/backup-cognito-id/src/__tests__/index.test.js
@@ -76,4 +76,81 @@ describe('handler', () => {
 
     consoleErrorSpy.mockRestore();
   });
+
+  it('should back up user to S3 successfully', async () => {
+    const event = {
+      eventName: 'CreateUser',
+      detail: {
+        requestParameters: { userPoolId: 'testPoolId' },
+        additionalEventData: { sub: 'testUser' },
+      },
+    };
+    process.env.S3_BUCKET_NAME = 'testBucket';
+
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation();
+    cognitoMock.on(AdminGetUserCommand).resolves({
+      UserAttributes: [
+        { Name: 'email', Value: 'test@example.com' },
+        { Name: 'name', Value: 'Test User' },
+      ],
+    });
+    s3Mock.on(PutObjectCommand).resolves({});
+
+    await handler(event);
+
+    expect(consoleInfoSpy).toHaveBeenCalledWith(
+      JSON.stringify({
+        level: 'info',
+        message: 'Successfully backed up user testUser to S3',
+        userName: 'testUser',
+        event: 'CreateUser',
+      })
+    );
+
+    consoleInfoSpy.mockRestore();
+  });
+
+  it('should correctly encapsulate user attributes containing commas', async () => {
+    const event = {
+      eventName: 'CreateUser',
+      detail: {
+        requestParameters: { userPoolId: 'testPoolId' },
+        additionalEventData: { sub: 'testUser' },
+      },
+    };
+    process.env.S3_BUCKET_NAME = 'testBucket';
+
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation();
+    cognitoMock.on(AdminGetUserCommand).resolves({
+      UserAttributes: [
+        { Name: 'email', Value: 'test@example.com' },
+        { Name: 'name', Value: 'Test, User' },
+        { Name: 'phone_number', Value: '+1234567890' },
+        {
+          Name: 'custom:abd_customAttrib',
+          Value: `'1234567890-=!@£$%^&*()_+;',.'`,
+        },
+      ],
+    });
+    s3Mock.on(PutObjectCommand).resolves({});
+
+    await handler(event);
+
+    expect(consoleInfoSpy).toHaveBeenCalledWith(
+      JSON.stringify({
+        level: 'info',
+        message: 'Successfully backed up user testUser to S3',
+        userName: 'testUser',
+        event: 'CreateUser',
+      })
+    );
+
+    const putObjectCall = s3Mock.calls(PutObjectCommand).pop();
+    const csvContent = putObjectCall.args[0].input.Body;
+    expect(csvContent).toBe(
+      `email,name,phone_number,custom:abd_customAttrib\n"test@example.com","Test, User","+1234567890","'1234567890-=!@£$%^&*()_+;',.'"`
+    );
+
+    consoleInfoSpy.mockRestore();
+  });
 });
