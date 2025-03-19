9b7a1edde8b961c4b9b6caf09a09cda831988eb7
Mark Ramsden <mark.ramsden5@nhs.net>
2025-03-19 11:45:58 +0000

    CCM-8898: review

--- infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
+++ infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
@@ -21,7 +21,6 @@ data "aws_iam_policy_document" "github_deploy" {
       "backup:*",
       "cloudformation:*",
       "cloudfront:*",
-      "cloudfront:*",
       "cognito-identity:*",
       "cognito-idp:*",
       "distribution:*",
