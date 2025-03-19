2bbb1be460d63d470c789763077c6ad49e0ab47f
Mark Ramsden <mark.ramsden5@nhs.net>
2025-03-19 11:19:44 +0000

    CCM-8898: public key bucket and cdn

--- infrastructure/terraform/components/sandbox/variables.tf
+++ infrastructure/terraform/components/sandbox/variables.tf
@@ -38,7 +38,7 @@ variable "group" {
 variable "component" {
   type        = string
   description = "The variable encapsulating the name of this component"
-  default     = "sandbox"
+  default     = "sbx"
 }
 
 variable "default_tags" {
--- infrastructure/terraform/modules/public-signing-keys/cloudfront_distribution.tf
+++ infrastructure/terraform/modules/public-signing-keys/cloudfront_distribution.tf
@@ -37,7 +37,6 @@ resource "aws_cloudfront_distribution" "main" {
     }
   }
 
-  # Github Web-CMS behaviour
   default_cache_behavior {
     allowed_methods = [
       "GET",
@@ -61,7 +60,7 @@ resource "aws_cloudfront_distribution" "main" {
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
-    max_ttl                = 86400
+    max_ttl                = 3600
     compress               = true
   }
 }
