dcd9c704f763fd6f3a8dfbe1a12e64af3f21701f
Mark Ramsden <mark.ramsden5@nhs.net>
2025-03-20 14:10:17 +0000

    CCM-8898: add versions to module

---
+++ infrastructure/terraform/modules/public-signing-keys/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.9.2"
+}
