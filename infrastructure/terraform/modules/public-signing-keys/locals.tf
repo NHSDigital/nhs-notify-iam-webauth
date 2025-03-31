8ad2e1af2f5f131ce04f6b512010b795042f7851
Mark Ramsden <mark.ramsden5@nhs.net>
2025-03-31 06:36:20 +0100

    CCM-8898: make public keys domain unique

--- infrastructure/terraform/modules/public-signing-keys/locals.tf
+++ infrastructure/terraform/modules/public-signing-keys/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  root_domain_name = "${var.environment}.${data.aws_route53_zone.main.name}" # e.g. [main|dev|abxy0].foo.[dev|nonprod|prod].nhsnotify.national.nhs.uk
+  root_domain_name = "jwks.${var.environment}.${data.aws_route53_zone.main.name}" # e.g. jwks.[main|dev|abxy0].foo.[dev|nonprod|prod].nhsnotify.national.nhs.uk
 }
