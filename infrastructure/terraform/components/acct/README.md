01a9cc11a53c38bdcc96ea7e2c5fe59261937794
jamesthompson26-nhs <115219893+jamesthompson26-nhs@users.noreply.github.com>
2025-05-09 16:27:21 +0100

    CCM-8197: Cross Account Observability_v2 (#268)

--- infrastructure/terraform/components/acct/README.md
+++ infrastructure/terraform/components/acct/README.md
@@ -42,6 +42,7 @@
 | <a name="output_dns_zone"></a> [dns\_zone](#output\_dns\_zone) | n/a |
 | <a name="output_github_pat_ssm_param_name"></a> [github\_pat\_ssm\_param\_name](#output\_github\_pat\_ssm\_param\_name) | n/a |
 | <a name="output_kms_sandbox_arn"></a> [kms\_sandbox\_arn](#output\_kms\_sandbox\_arn) | n/a |
+| <a name="output_log_subscription_role_arn"></a> [log\_subscription\_role\_arn](#output\_log\_subscription\_role\_arn) | n/a |
 | <a name="output_s3_buckets"></a> [s3\_buckets](#output\_s3\_buckets) | n/a |
 <!-- vale on -->
 <!-- markdownlint-enable -->
--- infrastructure/terraform/components/app/cloudwatch_log_group_amplify.tf
+++ infrastructure/terraform/components/app/cloudwatch_log_group_amplify.tf
@@ -2,3 +2,13 @@ resource "aws_cloudwatch_log_group" "amplify" {
   name              = "/aws/amplify/${aws_amplify_app.main.id}"
   retention_in_days = var.log_retention_in_days
 }
+
+resource "aws_cloudwatch_log_subscription_filter" "amplify" {
+  name            = "${local.csi}-amplify-${aws_amplify_app.main.id}"
+  log_group_name  = aws_cloudwatch_log_group.amplify.name
+  filter_pattern  = ""
+  destination_arn = "arn:aws:logs:${var.region}:${var.observability_account_id}:destination:nhs-notify-main-acct-firehose-logs"
+  role_arn        = local.acct.log_subscription_role_arn
+
+  depends_on = [aws_amplify_app.main]
+}
