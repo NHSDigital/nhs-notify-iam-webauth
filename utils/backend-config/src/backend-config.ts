6e893213b6b05d10423bcf014535dd317036911b
Mark Ramsden <mark.ramsden5@nhs.net>
2025-05-16 10:55:22 +0100

    CCM-9554: linting

--- utils/backend-config/src/backend-config.ts
+++ utils/backend-config/src/backend-config.ts
@@ -15,22 +15,24 @@ export type BackendConfig = {
 export const BackendConfigHelper = {
   fromTerraformOutputsFile(filepath: string): BackendConfig {
     const outputs = JSON.parse(fs.readFileSync(filepath, 'utf8'));
-    
+
     return {
       groupTag: outputs.group_tag.value,
       nameTag: outputs.name_tag.value,
-      keyDirectorySsmParameterName: outputs.key_directory_ssm_parameter_name.value,
+      keyDirectorySsmParameterName:
+        outputs.key_directory_ssm_parameter_name.value,
       keyRotationLambdaName: outputs.key_rotation_lambda_name.value,
       publicKeysS3BucketName: outputs.public_keys_s3_bucket_name.value,
       region: outputs.deployment.value.aws_region,
-      accountId: outputs.deployment.value.aws_account_id
+      accountId: outputs.deployment.value.aws_account_id,
     };
   },
 
   toEnv(config: BackendConfig): void {
     process.env.GROUP_TAG = config.groupTag;
     process.env.NAME_TAG = config.nameTag;
-    process.env.KEY_DIRECTORY_SSM_PARAMETER_NAME = config.keyDirectorySsmParameterName;
+    process.env.KEY_DIRECTORY_SSM_PARAMETER_NAME =
+      config.keyDirectorySsmParameterName;
     process.env.KEY_ROTATION_LAMBDA_NAME = config.keyRotationLambdaName;
     process.env.PUBLIC_KEYS_S3_BUCKET_NAME = config.publicKeysS3BucketName;
     process.env.ACCOUNT_ID = config.accountId;
