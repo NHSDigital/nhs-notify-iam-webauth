f16fec32be7fb88c230565fa495e19ce7f772db2
Chris Elliott <chris.elliott17@nhs.net>
2025-05-16 08:25:56 +0100

    CCM-8432: Lambda packaging

--- infrastructure/terraform/modules/lambda-function/cloudwatch_log_subscription_filter_firehose.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cloudwatch_log_subscription_filter" "firehose" {
-  count           = var.log_destination_arn != "" ? 1 : 0
-  name            = replace(aws_cloudwatch_log_group.lambda.name, "/", "-")
-  log_group_name  = aws_cloudwatch_log_group.lambda.name
-  filter_pattern  = var.filter_pattern
-  destination_arn = var.log_destination_arn
-  role_arn        = var.log_subscription_role_arn
-}
