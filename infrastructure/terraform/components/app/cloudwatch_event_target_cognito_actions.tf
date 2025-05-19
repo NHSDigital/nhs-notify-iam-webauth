8b64fef378a2f6fed3824f34337d3fdade1c8470
Aiden Vaines <54067008+aidenvaines-bjss@users.noreply.github.com>
2025-05-19 11:22:14 +0100

    CCM-10155 Fix Cognito Backup Trigger (#278)

--- infrastructure/terraform/components/app/cloudwatch_event_target_cognito_actions.tf
+++ infrastructure/terraform/components/app/cloudwatch_event_target_cognito_actions.tf
@@ -4,3 +4,10 @@ resource "aws_cloudwatch_event_target" "cognito_actions" {
   rule = aws_cloudwatch_event_rule.backup_cognito_id[0].name
   arn  = module.lambda_backup_cognito_id[0].function_arn
 }
+
+resource "aws_cloudwatch_event_target" "cognito_oauth_actions" {
+  count = var.destination_vault_arn != null ? 1 : 0
+
+  rule = aws_cloudwatch_event_rule.backup_cognito_id_oauth[0].name
+  arn  = module.lambda_backup_cognito_id[0].function_arn
+}
