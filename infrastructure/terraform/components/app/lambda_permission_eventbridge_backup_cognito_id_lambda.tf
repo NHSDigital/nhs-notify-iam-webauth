caae1bc7e79c6402980a59a24021574640df7dba
Aiden Vaines <54067008+aidenvaines-bjss@users.noreply.github.com>
2025-05-21 09:38:51 +0100

    CCM-10155 Fix Cognito Backup Trigger (#281)

--- infrastructure/terraform/components/app/lambda_permission_eventbridge_backup_cognito_id_lambda.tf
+++ infrastructure/terraform/components/app/lambda_permission_eventbridge_backup_cognito_id_lambda.tf
@@ -1,17 +1,17 @@
 resource "aws_lambda_permission" "eventbridge_backup_cognito_id_lambda" {
   count = var.destination_vault_arn != null ? 1 : 0
 
-  statement_id  = "AllowExecutionFromCloudWatch"
+  statement_id  = "AllowExecutionFromCloudtrailAPI"
   action        = "lambda:InvokeFunction"
   function_name = module.lambda_backup_cognito_id[0].function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.backup_cognito_id[0].arn
 }
 
-resource "aws_lambda_permission" "eventbridge_backup_outh_cognito_id_lambda" {
+resource "aws_lambda_permission" "eventbridge_backup_oauth_cognito_id_lambda" {
   count = var.destination_vault_arn != null ? 1 : 0
 
-  statement_id  = "AllowExecutionFromCloudWatch"
+  statement_id  = "AllowExecutionFromCloudtrailOauth"
   action        = "lambda:InvokeFunction"
   function_name = module.lambda_backup_cognito_id[0].function_name
   principal     = "events.amazonaws.com"
