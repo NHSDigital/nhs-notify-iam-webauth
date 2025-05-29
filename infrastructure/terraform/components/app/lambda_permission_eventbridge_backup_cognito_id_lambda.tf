22dc75a85f7832f79df8b2ec9454cb7dffcac2c8
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-05-29 11:34:23 +0100

    CCM-10155 single event trigger and handing

--- infrastructure/terraform/components/app/lambda_permission_eventbridge_backup_cognito_id_lambda.tf
+++ infrastructure/terraform/components/app/lambda_permission_eventbridge_backup_cognito_id_lambda.tf
@@ -7,13 +7,3 @@ resource "aws_lambda_permission" "eventbridge_backup_cognito_id_lambda" {
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.backup_cognito_id[0].arn
 }
-
-resource "aws_lambda_permission" "eventbridge_backup_oauth_cognito_id_lambda" {
-  count = var.destination_vault_arn != null ? 1 : 0
-
-  statement_id  = "AllowExecutionFromCloudtrailOauth"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda_backup_cognito_id[0].function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.backup_cognito_id_oauth[0].arn
-}
