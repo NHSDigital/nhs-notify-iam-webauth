d5eeba356eb5069a6fc230aa945d2add7d69b29b
Mark Ramsden <mark.ramsden5@nhs.net>
2025-07-03 13:47:09 +0100

    CCM-10294: move over to contextual tests

--- infrastructure/terraform/components/sandbox/pre.sh
+++ infrastructure/terraform/components/sandbox/pre.sh
@@ -1,6 +1,19 @@
+REGION=$1
+ENVIRONMENT=$2
+ACTION=$3
+
 echo Running pre.sh
+echo "REGION=$REGION"
+echo "ENVIRONMENT=$ENVIRONMENT"
+echo "ACTION=$ACTION"
 
-if [ -z "$SKIP_SANDBOX_INSTALL" ]; then npm ci; fi
+if [ "${ACTION}" == "apply" ]; then
+    echo "Building lambdas for distribution"
 
-echo About to build lambdas
-npm run build-lambda
+    if [ -z "$SKIP_SANDBOX_INSTALL" ]; then npm ci; fi
+
+    echo About to build lambdas
+    npm run build-lambda
+else
+    echo "Skipping lambda build for action $ACTION"
+fi
