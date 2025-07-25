520e380915e39fb80162c75e0ddb46cf1554cd17
Alex Nuttall <alex.nuttall1@nhs.net>
2025-07-25 08:38:38 +0100

    oauth2 page

--- lambdas/cognito-triggers/src/pre-authentication.ts
+++ lambdas/cognito-triggers/src/pre-authentication.ts
@@ -2,13 +2,9 @@ import type { PreAuthenticationTriggerEvent } from 'aws-lambda';
 
 export class PreAuthenticationLambda {
   handler = async (event: PreAuthenticationTriggerEvent) => {
-    const response = { ...event };
-
-    console.dir(event, { depth: Infinity });
-
     if (process.env.THROW) throw new Error('Stop!');
 
-    return response;
+    return event;
   };
 }
 
