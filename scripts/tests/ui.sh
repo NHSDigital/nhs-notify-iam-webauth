6770a24f3993b029b7b3591e0e602811dd9dd7e9
Mark Ramsden <mark.ramsden5@nhs.net>
2025-07-29 15:05:40 +0100

    CCM-10980: test negative cases

--- scripts/tests/ui.sh
+++ scripts/tests/ui.sh
@@ -12,6 +12,6 @@ echo "TEST_EXIT_CODE=$TEST_EXIT_CODE"
 
 mkdir -p ./tests/acceptance-test-report
 cp -r ./tests/test-team/playwright-report-component-tests ./tests/acceptance-test-report
-[[ -e ./tests/test-team/test-results ]] && cp -r ./tests/test-team/test-results ../acceptance-test-report
+[[ -e ./tests/test-team/test-results ]] && cp -r ./tests/test-team/test-results ./tests/acceptance-test-report
 
 exit $TEST_EXIT_CODE
