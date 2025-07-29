8fb93e1c63eabd4efb4e190d8b16f028fd6660ba
Mark Ramsden <mark.ramsden5@nhs.net>
2025-07-29 12:18:02 +0100

    CCM-10980: debugging

--- scripts/tests/accessibility.sh
+++ scripts/tests/accessibility.sh
@@ -5,15 +5,13 @@ set -euo pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 npm run build
-
 npm run app:start -w frontend
-
 npm run app:wait -w frontend
 
 TEST_EXIT_CODE=0
 npm run test:accessibility || TEST_EXIT_CODE=$?
 echo "TEST_EXIT_CODE=$TEST_EXIT_CODE"
-mkdir -p ../acceptance-test-report
+mkdir -p ./tests/acceptance-test-report
 cp -r tests/accessibility/.reports/accessibility tests/acceptance-test-report
 
 npm run app:stop -w frontend
--- scripts/tests/ui.sh
+++ scripts/tests/ui.sh
@@ -10,8 +10,8 @@ TEST_EXIT_CODE=0
 npm run test:ui || TEST_EXIT_CODE=$?
 echo "TEST_EXIT_CODE=$TEST_EXIT_CODE"
 
-mkdir -p ../acceptance-test-report
-cp -r ./playwright-report-component-tests ../acceptance-test-report
-[[ -e test-results ]] && cp -r ./test-results ../acceptance-test-report
+mkdir -p ./tests/acceptance-test-report
+cp -r ./tests/test-team/playwright-report-component-tests ./tests/acceptance-test-report
+[[ -e test-results ]] && cp -r ./tests/test-team/test-results ../acceptance-test-report
 
 exit $TEST_EXIT_CODE
