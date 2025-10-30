66ccfa0d6b3444ed9fe77c22833dde677b6ebd9f
Mark Ramsden <mark.ramsden5@nhs.net>
2025-10-30 09:22:06 +0000

    CCM-11890: de-couple external username - wip

--- infrastructure/terraform/components/app/cognito_user_pool.tf
+++ infrastructure/terraform/components/app/cognito_user_pool.tf
@@ -58,7 +58,7 @@ resource "aws_cognito_user_pool" "main" {
     name                     = "nhs_notify_user_id"
     attribute_data_type      = "String"
     developer_only_attribute = false
-    mutable                  = false
+    mutable                  = true
     required                 = false
     string_attribute_constraints {
       min_length = 0
