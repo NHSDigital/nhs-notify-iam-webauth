37fb818e0ea2fc4add17a976b5ff9bdad405f646
Nicki Derrick <nicki.derrick1@nhs.net>
2025-11-03 16:08:23 +0000

    CCM-10814 WIP

--- frontend/src/__tests__/app/signin/route.test.ts
+++ frontend/src/__tests__/app/signin/route.test.ts
@@ -31,7 +31,7 @@ test('returns redirect', async () => {
     secure: true,
   });
   expect(response.status).toEqual(307);
-  expect(response.headers.get('Location')).toEqual('/redirect-url');
+  expect(response.headers.get('Location')).toEqual('http://localhost:3000/redirect-url');
 });
 
 test('returns redirect - sanitizes redirect path', async () => {
@@ -43,7 +43,7 @@ test('returns redirect - sanitizes redirect path', async () => {
   const response = await GET(request);
 
   expect(response.status).toEqual(307);
-  expect(response.headers.get('Location')).toEqual('/redirect-url');
+  expect(response.headers.get('Location')).toEqual('http://localhost:3000/redirect-url');
 });
 
 test('returns redirect to /templates/message-templates if no redirect given', async () => {
@@ -56,7 +56,7 @@ test('returns redirect to /templates/message-templates if no redirect given', as
 
   expect(response.status).toEqual(307);
   expect(response.headers.get('Location')).toEqual(
-    '/templates/message-templates'
+    'http://localhost:3000/templates/message-templates'
   );
 });
 
@@ -71,7 +71,7 @@ test('returns redirect to /auth if no session detected', async () => {
   expect(cookiesMock.delete).toHaveBeenCalledWith('csrf_token');
 
   expect(response.status).toEqual(307);
-  expect(response.headers.get('Location')).toEqual('/auth');
+  expect(response.headers.get('Location')).toEqual('https://test/auth');
 });
 
 test('retains redirect search param on /auth redirect', async () => {
@@ -86,6 +86,6 @@ test('retains redirect search param on /auth redirect', async () => {
 
   expect(response.status).toEqual(307);
   expect(response.headers.get('Location')).toEqual(
-    '/auth?redirect=%2Fredirect-path'
+    'https://test/auth?redirect=%2Fredirect-path'
   );
 });
