b0bf941112fcbfa207879d9f034da738e6f7d406
Mark Ramsden <mark.ramsden5@nhs.net>
2026-02-12 13:59:36 +0000

    CCM-11906: update totp script

--- scripts/totp.ts
+++ scripts/totp.ts
@@ -1,19 +1,27 @@
 import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
-import { TOTP } from "totp-generator"
+import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm';
+import { TOTP } from "totp-generator";
+
+const ssmClient = new SSMClient({
+  region: process.env.REGION,
+  retryMode: 'standard',
+  maxAttempts: 10,
+});
 
 // This script is for generating an OTP for manual CIS2 login in a Static Environment
 // Do not use it for test automation or in sandbox environments!
 (async function main(){
-  const { SecretString } = await new SecretsManagerClient().send(new GetSecretValueCommand({
-    SecretId: 'test/cis2-int/credentials',
-  }))
+  const { Parameter } = await new SSMClient().send(new GetParameterCommand({
+    Name: '/test/cis2-int/credentials',
+    WithDecryption: true,
+  }));
+  const parameterValue = Parameter?.Value;
 
-  if (!SecretString) {
-    throw new Error('No SecretString returned from Secrets Manager.')
+  if (!parameterValue) {
+    throw new Error('No Value returned from SSM Parameter Store.')
   }
 
-  const { totpSecret } = JSON.parse(SecretString);
-
+  const { totpSecret } = JSON.parse(parameterValue);
   if (!totpSecret) {
     throw new Error('TOTP Secret not found in Secrets Manager.')
   }
