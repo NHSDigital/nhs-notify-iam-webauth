aebee6d5e145459f547f5a0ead2b85316102f894
bhansell1 <ben.hansell1@nhs.net>
2026-03-05 12:08:27 +0000

    use policy_document instead of inline policie

--- infrastructure/terraform/modules/cognito-triggers/iam_role_ssm_automation_allocate_user_to_client.tf
+++ infrastructure/terraform/modules/cognito-triggers/iam_role_ssm_automation_allocate_user_to_client.tf
@@ -1,76 +1,81 @@
+data "aws_iam_policy_document" "ssm_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ssm.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ssm_automation_allocate_user" {
   name        = "${var.csi}-ssm-automation-allocate-user"
   description = "Execution role for SSM Automation document that allocates users to clients"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ssm.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.ssm_assume_role.json
 }
+
+data "aws_iam_policy_document" "ssm_automation_allocate_user" {
+  statement {
+    sid     = "CognitoUserReadAccess"
+    effect  = "Allow"
+    actions = ["cognito-idp:AdminGetUser"]
+    resources = [
+      "arn:aws:cognito-idp:${var.region}:${var.aws_account_id}:userpool/${var.user_pool_id}"
+    ]
+  }
+
+  statement {
+    sid     = "CognitoUserWriteAccess"
+    effect  = "Allow"
+    actions = ["cognito-idp:AdminUpdateUserAttributes"]
+    resources = [
+      "arn:aws:cognito-idp:${var.region}:${var.aws_account_id}:userpool/${var.user_pool_id}"
+    ]
+  }
+
+  statement {
+    sid    = "DynamoDBWriteAccess"
+    effect = "Allow"
+    actions = [
+      "dynamodb:ConditionCheckItem",
+      "dynamodb:PutItem",
+      "dynamodb:TransactWriteItems",
+    ]
+    resources = [aws_dynamodb_table.users.arn]
+  }
+
+  statement {
+    sid    = "KMSAccessForDynamoDBEncryption"
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKey*",
+      "kms:ReEncrypt*",
+    ]
+    resources = [var.kms_key_arn]
+  }
+
+  statement {
+    sid     = "SSMClientParameterReadAccess"
+    effect  = "Allow"
+    actions = ["ssm:GetParameter"]
+    resources = [
+      "arn:aws:ssm:${var.region}:${var.aws_account_id}:parameter/${var.csi}/clients/*"
+    ]
+  }
+}
+
 
 resource "aws_iam_policy" "ssm_automation_allocate_user" {
   name        = "${var.csi}-ssm-automation-allocate-user"
   description = "Permissions for SSM Automation to allocate users to clients"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "CognitoUserReadAccess"
-        Effect = "Allow"
-        Action = [
-          "cognito-idp:AdminGetUser"
-        ]
-        Resource = "arn:aws:cognito-idp:${var.region}:${var.aws_account_id}:userpool/${var.user_pool_id}"
-      },
-      {
-        Sid    = "CognitoUserWriteAccess"
-        Effect = "Allow"
-        Action = [
-          "cognito-idp:AdminUpdateUserAttributes"
-        ]
-        Resource = "arn:aws:cognito-idp:${var.region}:${var.aws_account_id}:userpool/${var.user_pool_id}"
-      },
-      {
-        Sid    = "DynamoDBWriteAccess"
-        Effect = "Allow"
-        Action = [
-          "dynamodb:ConditionCheckItem",
-          "dynamodb:PutItem",
-          "dynamodb:TransactWriteItems"
-        ]
-        Resource = aws_dynamodb_table.users.arn
-      },
-      {
-        Sid    = "KMSAccessForDynamoDBEncryption"
-        Effect = "Allow"
-        Action = [
-          "kms:Decrypt",
-          "kms:DescribeKey",
-          "kms:Encrypt",
-          "kms:GenerateDataKey*",
-          "kms:ReEncrypt*"
-        ]
-        Resource = var.kms_key_arn
-      },
-      {
-        Sid    = "SSMClientParameterReadAccess"
-        Effect = "Allow"
-        Action = [
-          "ssm:GetParameter"
-        ]
-        Resource = "arn:aws:ssm:${var.region}:${var.aws_account_id}:parameter/${var.csi}/clients/*"
-      },
-    ]
-  })
+  policy = data.aws_iam_policy_document.ssm_automation_allocate_user.json
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_automation_allocate_user" {
