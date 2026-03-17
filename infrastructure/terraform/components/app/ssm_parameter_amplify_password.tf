4c54def5efa5a87b9f654373bef9300aabcab1ff
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-03-17 13:48:56 +0000

    CCM-14605: Encrypt SSM Parameter

--- infrastructure/terraform/components/app/ssm_parameter_amplify_password.tf
+++ infrastructure/terraform/components/app/ssm_parameter_amplify_password.tf
@@ -4,7 +4,7 @@ resource "aws_ssm_parameter" "amplify_password" {
   name        = "/${local.csi}/amplify_password"
   description = "The Basic Auth password used for the amplify app. This parameter is sourced from Github Environment variables"
 
-  type  = "String"
+  type  = "SecureString"
   value = var.AMPLIFY_BASIC_AUTH_SECRET != "unset" ? var.AMPLIFY_BASIC_AUTH_SECRET : random_password.password[0].result
 }
 
